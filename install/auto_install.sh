546188b86fce9905d97cc528d14a987d7eaf9321
Forbidden-Equation® <59021489+F0rbidden-Equation@users.noreply.github.com>
2021-01-20 18:48:02 +0100

    Add files via upload

---
+++ install/auto_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#apt-get update 
+sudo apt-get install git python3 python-pip python3-pip 
+sudo apt-get install nmap
+sudo apt-get install python3-nmap
+sudo apt-get install nikto
+sudo apt-get install wkhtmltopdf
+sudo apt-get install python3-tk
+
+pip3 install PyQt5==5.13
+pip3 install PyQtWebEngine
+
+python3 Install_Hyperionv3.py
+
