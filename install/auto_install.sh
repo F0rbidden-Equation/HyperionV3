cd7140eed3ca8241d8961a1c2a51c84f6f8bc0ab
Forbidden-Equation® <59021489+F0rbidden-Equation@users.noreply.github.com>
2021-01-22 18:43:45 +0100

    Update auto_install.sh

--- install/auto_install.sh
+++ install/auto_install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #apt-get update 
-sudo apt-get install git python3 python-pip python3-pip 
+sudo apt-get install git python3 python-pip 
 sudo apt-get install nmap
 sudo apt-get install python3-nmap
 sudo apt-get install nikto
 sudo apt-get install wkhtmltopdf
 sudo apt-get install python3-tk
-
+sudo apt install python3-pip
+sudo apt install git
+sudo apt install xsltproc
 pip3 install PyQt5==5.13
 pip3 install PyQtWebEngine
-
+pip3 install python3-nmap
 python3 Install_Hyperionv3.py
-
