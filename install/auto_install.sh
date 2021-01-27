19245673ce03a87c25d178b8b1e474487573b2aa
Forbidden-Equation® <59021489+F0rbidden-Equation@users.noreply.github.com>
2021-01-27 22:57:55 +0100

    Update auto_install.sh

--- install/auto_install.sh
+++ install/auto_install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #apt-get update 
-sudo apt-get install git python3 python-pip 
+sudo apt-get install git  
+sudo apt-get install python3
+sudo apt-get install python3-pip 
 sudo apt-get install nmap
-sudo apt-get install python3-nmap
+#sudo apt-get install python3-nmap
 sudo apt-get install nikto
 sudo apt-get install wkhtmltopdf
 sudo apt-get install python3-tk
