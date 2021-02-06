62056206eafa2a010eeeda01bf2923074bfed256
Forbidden-Equation® <59021489+F0rbidden-Equation@users.noreply.github.com>
2021-02-06 20:10:19 +0100

    Update auto_install.sh

--- install/auto_install.sh
+++ install/auto_install.sh
@@ -11,7 +11,9 @@ sudo apt-get install python3-tk
 sudo apt install python3-pip
 sudo apt install git
 sudo apt install xsltproc
-pip3 install PyQt5==5.13
-pip3 install PyQtWebEngine==5.15.0
+apt-get install python3-pyqt5
+apt-get install python3-pyqt5.qtwebengine.
+#pip3 install PyQt5==5.13
+#pip3 install PyQtWebEngine==5.15.0
 pip3 install python3-nmap
 python3 Install_Hyperionv3.py
